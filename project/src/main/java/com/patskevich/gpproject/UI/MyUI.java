b95474924e49c3fd7d37626a1744230fe4b515b6
Stanislav Patskevich <46021729+PatskevichS@users.noreply.github.com>
2018-12-31 18:57:56 +0300

    Add files via upload

--- project/src/main/java/com/patskevich/gpproject/UI/MyUI.java
+++ project/src/main/java/com/patskevich/gpproject/UI/MyUI.java
@@ -1,11 +1,10 @@
 package com.patskevich.gpproject.UI;
 
 import com.patskevich.gpproject.dto.MessageDto.MessageOutputDto;
-import com.patskevich.gpproject.entity.Room;
-import com.patskevich.gpproject.service.MessageService;
-import com.patskevich.gpproject.service.RoomService;
-import com.patskevich.gpproject.service.UserService;
+import com.patskevich.gpproject.service.LogoutService;
+import com.patskevich.gpproject.service.UiService;
 import com.vaadin.annotations.Theme;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import com.vaadin.spring.annotation.SpringUI;
@@ -16,32 +15,27 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @SpringUI
 @Theme("mytheme")
 public class MyUI extends UI {
-
     private AdminForm adminForm = new AdminForm(this);
     private UserForm userForm = new UserForm(this);
-    private Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-    @Autowired
-    private RoomService roomService;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private MessageService messageService;
-
-    final VerticalLayout layout = new VerticalLayout();
+    public Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    final VerticalLayout mainLayout = new VerticalLayout();
     final Label userInfo = new Label();
-    final Label log = new Label();
+    final Label log = new Label("LOG: ");
     final Label description = new Label();
-    final ComboBox<String> roomSelected= new ComboBox<>();
+    final ComboBox<String> roomSelected = new ComboBox<>("Room:");
     final Grid<MessageOutputDto> messageGrid = new Grid<>(MessageOutputDto.class);
     final TextField inputTextField = new TextField();
 
+    @Autowired
+    private UiService uiService;
+
+    @Autowired
+    private LogoutService logoutService;
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        layout.setWidth("100%");
-        setContent(layout);
+        mainLayout.setWidth("100%");
+        setContent(mainLayout);
         adminForm.setVisible(false);
         userForm.setVisible(false);
         setHeightUndefined();
@@ -51,97 +45,105 @@ public class MyUI extends UI {
         addGridPanel();
     }
 
-    private void addMainMenu(){
-        MenuBar menuBar = new MenuBar();
-        MenuBar.MenuItem admin = menuBar.addItem("admin");
-        MenuBar.MenuItem settings = menuBar.addItem("settings");
-        MenuBar.MenuItem logout = menuBar.addItem("logout");
-        admin.setCommand(e->{
-            if (adminForm.isVisible()) adminForm.setVisible(false);
-            else adminForm.setVisible(true);
+    private void addMainMenu() {
+        final MenuBar menuBar = new MenuBar();
+        final  MenuBar.MenuItem admin = menuBar.addItem("admin");
+        final  MenuBar.MenuItem settings = menuBar.addItem("settings");
+        final MenuBar.MenuItem logout = menuBar.addItem("logout");
+        admin.setIcon(VaadinIcons.NURSE);
+        settings.setIcon(VaadinIcons.OPTIONS);
+        logout.setIcon(VaadinIcons.EXIT);
+        admin.setCommand(e -> {
+            if (adminForm.isVisible()) {
+                adminForm.setVisible(false);
+                userForm.setVisible(false);
+                messageGrid.setVisible(true);
+            } else {
+                adminForm.setUiService(uiService);
+                adminForm.setV(true);
+                messageGrid.setVisible(false);
+                userForm.setVisible(false);
+            }
         });
-        settings.setCommand(e->{
-            if (userForm.isVisible()) userForm.setVisible(false);
-            else userForm.setVisible(true);
+        logout.setCommand(e -> {
+            uiService.updateLog(log, "На данный момент эта функция недоступна");
         });
-        if (userService.getUser(authentication.getName()).getRole().equals("ROLE_ADMIN"))
+        settings.setCommand(e -> {
+            if (userForm.isVisible()) {
+                userForm.setVisible(false);
+                messageGrid.setVisible(true);
+                adminForm.setVisible(false);
+            } else {
+                userForm.setUiService(uiService);
+                userForm.setV(true);
+                messageGrid.setVisible(true);
+                adminForm.setVisible(false);
+            }
+        });
+        if (uiService.getUserRole(auth.getName()).equals("ROLE_ADMIN"))
             admin.setVisible(true);
         else admin.setVisible(false);
-        layout.addComponent(menuBar);
-        layout.setComponentAlignment(menuBar, Alignment.BOTTOM_RIGHT);
+        mainLayout.addComponent(menuBar);
+        mainLayout.setComponentAlignment(menuBar, Alignment.BOTTOM_RIGHT);
     }
 
-    private void addSelectedRoom(){
-        HorizontalLayout infoAndLog = new HorizontalLayout();
-        HorizontalLayout selectedRoom = new HorizontalLayout();
-        VerticalLayout verticalLayout = new VerticalLayout();
+    private void addSelectedRoom() {
+        final  HorizontalLayout infoAndLog = new HorizontalLayout();
+        final  HorizontalLayout selectedRoom = new HorizontalLayout();
+        final VerticalLayout verticalLayout = new VerticalLayout();
         verticalLayout.setMargin(false);
-        userInfo.setValue("Name: "+authentication.getName()+" | Role: "+authentication.getAuthorities());
-        log.setValue("LOG: ");
-        roomSelected.setCaption("Room:");
-        updateRoomSelected(roomSelected);
-        roomSelected.addValueChangeListener(e->{
-            if (!roomSelected.isSelected("")) {
-                updateLog(userService.changeRoom(roomSelected.getSelectedItem().get()));
-                updateGrid(userService.getUser(authentication.getName()).getRoom());
-            }
-            else updateLog("");
+        uiService.updateUserInfo(userInfo, auth.getName());
+        uiService.updateRoomSelected(roomSelected, auth.getName());
+        roomSelected.addValueChangeListener(e -> {
+            if (!roomSelected.isEmpty()) {
+                uiService.updateLog(log, uiService.changeRoom(roomSelected.getSelectedItem().get(), auth.getName()));
+                uiService.updateGrid(auth.getName(), messageGrid);
+                uiService.updateRoomDecription(description, auth.getName());
+            } else uiService.updateLog(log, "");
         });
-        description.setValue("Description: "+userService.getUser(authentication.getName()).getRoom().getDescription());
-        infoAndLog.addComponents(userInfo,log);
-        selectedRoom.addComponents(roomSelected,description);
+        uiService.updateRoomDecription(description, auth.getName());
+        infoAndLog.addComponents(userInfo, log);
+        selectedRoom.addComponents(roomSelected, description);
         selectedRoom.setComponentAlignment(description, Alignment.BOTTOM_LEFT);
-        verticalLayout.addComponents(infoAndLog,selectedRoom);
-        layout.addComponents(verticalLayout);
+        verticalLayout.addComponents(infoAndLog, selectedRoom);
+        mainLayout.addComponents(verticalLayout);
     }
 
-    private void addInputTextLayout(){
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        Button inputTextButton = new Button("Отправить");
-        inputTextButton.addClickListener(e-> {
-            if (!inputTextField.getValue().equals("")){
-                updateLog(messageService.addMessage(inputTextField.getValue()));
-                updateGrid(userService.getUser(authentication.getName()).getRoom());
-            }
-            else updateLog("Введите текст!");
+    private void addInputTextLayout() {
+        final HorizontalLayout horizontalLayout = new HorizontalLayout();
+        final Button inputTextButton = new Button("Отправить");
+        inputTextButton.setIcon(VaadinIcons.ENVELOPE);
+        inputTextButton.addClickListener(e -> {
+            if (!inputTextField.getValue().equals("")) {
+                uiService.updateLog(log, uiService.addMessage(inputTextField.getValue(), auth.getName()));
+                uiService.updateGrid(auth.getName(), messageGrid);
+            } else uiService.updateLog(log, "Введите текст!");
         });
         inputTextButton.setWidth("100%");
-        inputTextField.setWidth("1165");
-        horizontalLayout.addComponents(inputTextButton,inputTextField);
+        inputTextField.setWidth("1143");
+        horizontalLayout.addComponents(inputTextButton, inputTextField);
         horizontalLayout.setExpandRatio(inputTextButton, 1);
         horizontalLayout.setWidthUndefined();
-        layout.addComponents(horizontalLayout);
+        mainLayout.addComponents(horizontalLayout);
     }
 
-    private void addGridPanel(){
-        messageGrid.setColumns("author","message");
+    private void addGridPanel() {
+        messageGrid.setColumns("author", "date", "message");
         messageGrid.setFrozenColumnCount(1);
+        messageGrid.getColumn("author").setMinimumWidth(150);
         messageGrid.getColumn("author").setWidth(150);
-        messageGrid.getColumn("message").setExpandRatio(1);
-        updateGrid(userService.getUser(authentication.getName()).getRoom());
+        messageGrid.getColumn("date").setMinimumWidth(150);
+        messageGrid.getColumn("date").setWidth(150);
+        uiService.updateGrid(auth.getName(), messageGrid);
         adminForm.setMargin(false);
         userForm.setMargin(false);
-        HorizontalLayout gridPanel = new HorizontalLayout(messageGrid,adminForm,userForm);
+        final HorizontalLayout gridPanel = new HorizontalLayout(messageGrid, adminForm, userForm);
         gridPanel.setSizeFull();
         messageGrid.setSizeFull();
         gridPanel.setExpandRatio(messageGrid, 1);
-        layout.addComponents(gridPanel);
+        gridPanel.setExpandRatio(adminForm, 1);
+        gridPanel.setComponentAlignment(adminForm, Alignment.MIDDLE_CENTER);
+        mainLayout.addComponents(gridPanel);
     }
 
-    private void updateGrid(Room room){
-        messageGrid.setItems(messageService.getRoomMesage(room.getName()));
-    }
-
-    private void updateRoomSelected(ComboBox comboBox){
-        comboBox.setItems(roomService.getNameRoomList());
-        comboBox.setSelectedItem(userService.getUser(authentication.getName()).getRoom().getName());
-    }
-
-    private void updateLog(String text){
-        log.setValue("LOG: "+text);
-    }
- //   @WebServlet(urlPatterns = "/vaadin/", name = "MyUIServlet", asyncSupported = true)
-  //  @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
- //   public static class MyUIServlet extends VaadinServlet {
- //   }
 }
--- project/src/main/java/com/patskevich/gpproject/UI/UserForm.java
+++ project/src/main/java/com/patskevich/gpproject/UI/UserForm.java
@@ -1,30 +1,49 @@
 package com.patskevich.gpproject.UI;
 
-import com.vaadin.ui.*;
 
+import com.patskevich.gpproject.service.UiService;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.*;
 public class UserForm extends FormLayout {
+
     private MyUI myUI;
     private VerticalLayout verticalLayout = new VerticalLayout();
+    private TextField name = new TextField("Nickname");
+    private Button update = new Button("Изменить");
+    private Label nickname = new Label("Nickname: ");
+    private Label log = new Label("LOG: ");
+
+    private UiService uiService;
 
     public UserForm(MyUI myUI){
         this.myUI = myUI;
         setSizeUndefined();
-        TextField textField1 = new TextField("name");
-        Label label = new Label("Role");
-        TextField textField2 = new TextField("password");
-        TextField textField3 = new TextField("new password");
-        Button button = new Button("update");
-        button.addClickListener(e-> setVisible(false));
-        Label label2 = new Label("LOG:");
-        verticalLayout.addComponents(textField1,label,textField2,textField3,button,label2);
-        Panel panel = new Panel("User Panel");
+        update.addClickListener(e->{
+           if (!name.isEmpty()){
+               uiService.changeNickname(myUI.auth.getName(), name.getValue());
+               uiService.updateGrid(myUI.auth.getName(), myUI.messageGrid);
+               uiService.updateLog(log, "Успешно");
+               nickname.setValue("Nickname: "+uiService.getNickname(myUI.auth.getName()));
+           } else uiService.updateLog(log,"Введите данные!");
+        });
+        update.setIcon(VaadinIcons.COG);
+        verticalLayout.addComponents(name,nickname,update,log);
+        verticalLayout.setComponentAlignment(update, Alignment.MIDDLE_CENTER);
+        final Panel panel = new Panel("User settings :");
         panel.setHeight("400");
         panel.setContent(verticalLayout);
         this.addComponent(panel);
     }
 
-    public void setV(){
-        setVisible(true);
+    public void setUiService(UiService uiService){
+        this.uiService = uiService;
     }
-}
 
+    public void setV(final Boolean visible){
+        uiService.updateLog(log,"");
+        nickname.setValue("Nickname: "+uiService.getNickname(myUI.auth.getName()));
+        name.setValue("");
+        this.setVisible(visible);
+    }
+
+}
