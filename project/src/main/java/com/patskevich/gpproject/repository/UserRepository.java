824d8debd3f6b7a0a94d5d3202d02ee21e044602
Stanislav Patskevich <46021729+PatskevichS@users.noreply.github.com>
2019-01-09 20:45:04 +0300

    Update UserRepository.java

--- project/src/main/java/com/patskevich/gpproject/repository/UserRepository.java
+++ project/src/main/java/com/patskevich/gpproject/repository/UserRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByName(String name);
+    User findByLogin(String name);
     User findByNickname(String nickname);
-    boolean existsByName (String name);
+    boolean existsByLogin (final String name);
     boolean existsByNickname(String name);
     List<User> findAllByRoom(Room room);
     User getById(long id);
